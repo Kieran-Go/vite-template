c65ed9023927061997aee7bfb9672b819a7fcc97
Kieran-Go <187461911+Kieran-Go@users.noreply.github.com>
2025-03-11 05:55:04 +1030

    Remove default React/Vite page
    - The default page, including its styles have been removed, leaving
    only a blank template.

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import './css/normalize.css'
+import './css/index.css'
+import App from './scripts/App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
